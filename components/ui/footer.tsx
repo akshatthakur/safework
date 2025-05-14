9c3decae96f8cab186e158ad541ea9851d4df82c
Akshat Thakur <79204593+akshatthakur@users.noreply.github.com>
2025-05-13 18:28:08 -0700

    Update footer.tsx

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -15,7 +15,7 @@ export default function Footer({ border = false }: { border?: boolean }) {
               <Logo />
             </div>
             <div className="text-sm text-gray-600">
-              &copy; safework.ai - All rights reserved.
+              &copy; SafeWorkAI - All rights reserved.
             </div>
           </div>
         </div>
